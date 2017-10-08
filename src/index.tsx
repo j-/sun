8c7563827a3affe8d677a9cb91404bc0315d7ad2
j- <j@skeoh.com>
2017-10-08 15:26:57 +1100

    Add Redux devtools extension support

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { Provider as StoreProvider } from 'react-redux';
 import rootReducer from './store';
 import './index.css';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, devToolsEnhancer({}));
 
 ReactDOM.render(
 	<StoreProvider store={store}>
