3e55c75544bc80cc0694e066020cfb7d555525a2
j- <j@skeoh.com>
2017-10-08 15:25:07 +1100

    Add store provider to app

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider as StoreProvider } from 'react-redux';
+import rootReducer from './store';
 import './index.css';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-	<App />,
+	<StoreProvider store={store}>
+		<App />
+	</StoreProvider>,
 	document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
