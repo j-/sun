9909902008576c1de526ec206b1969f5368fed1c
j- <j@skeoh.com>
2017-10-08 17:34:15 +1100

    Increase frequency to 15 minutes

--- src/containers/SunChart.ts
+++ src/containers/SunChart.ts
@@ -13,7 +13,7 @@ interface StateProps {
 const mapStateToProps = (state: ReducerState): StateProps => ({
 	startDate: new Date(2017, 9, 7, 6, 0, 0, 0),
 	endDate: new Date(2017, 9, 7, 20, 0, 0, 0),
-	interval: 1000 * 60 * 30, // 30 mins
+	interval: 1000 * 60 * 15, // 15 mins
 	getAltitude: (date: Date) => (
 		getSunAltitude(
 			date,
