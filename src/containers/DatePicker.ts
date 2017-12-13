e24d9c9cb1781e8bdcd320af373106a22b8b0e43
j- <j@skeoh.com>
2017-12-13 19:27:12 +1100

    Compare current time with midnight before start / after end

--- src/containers/DatePicker.ts
+++ src/containers/DatePicker.ts
@@ -17,12 +17,34 @@ interface DispatchProps {
 	onClickToday: () => void;
 }
 
+const getMidnightBefore = (input: Date): Date => {
+	const output = new Date(input);
+	output.setHours(0);
+	output.setMinutes(0);
+	output.setSeconds(0);
+	output.setMilliseconds(0);
+	return output;
+};
+
+const getMidnightAfter = (input: Date): Date => {
+	const output = getMidnightBefore(input);
+	output.setDate(output.getDate() + 1);
+	return output;
+};
+
 const mapStateToProps: MapStateToProps<StateProps, OwnProps> = (state: ReducerState): StateProps => {
 	const currentTime = getCurrentTime(state);
 	const startDate = getStartDate(state);
 	const endDate = getEndDate(state);
+	if (!currentTime || !startDate || !endDate) {
+		return {
+			isToday: null,
+		};
+	}
+	const startDateMorning = getMidnightBefore(startDate);
+	const endDateEvening = getMidnightAfter(endDate);
 	return {
-		isToday: currentTime && startDate && endDate && (currentTime > startDate) && (currentTime < endDate),
+		isToday: (currentTime > startDateMorning) && (currentTime < endDateEvening),
 	};
 };
 
